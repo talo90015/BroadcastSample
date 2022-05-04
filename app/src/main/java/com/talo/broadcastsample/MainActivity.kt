389350aabe83cab0717322ffd5544fd21999b070
Talo <talo90015@gmail.com>
2022-05-04 21:23:26 +0800

    init command

---
+++ app/src/main/java/com/talo/broadcastsample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.talo.broadcastsample
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
